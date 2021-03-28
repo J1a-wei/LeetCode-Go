a5cc08fb7406f87e7393c1e822f5a1cd2c3018c6
skrbug <skrbug@outlook.com>
2021-03-28 18:17:18 +0800

    update

--- 590_n_ary_tree_postorder_traversal/leetcode590.go
+++ 590_n_ary_tree_postorder_traversal/leetcode590.go
@@ -1,7 +1,5 @@
 package leetcode590
 
-
-
 type Node struct {
 	Val      int
 	Children []*Node
@@ -23,7 +21,7 @@ func helper(root *Node, ret *[]int) {
 }
 
 // 先序遍历是中左右， 后序遍历是 左右中
-// 那么我们调整一下先序遍历， 让其变成 中右左， 然后把结果数组翻转，输出的结果就是左右中了
+// 那么我们调整一下先序遍历， 让其变成 中右左(在栈中，依次遍历子节点就能完成这个操作) ， 然后把结果数组翻转，输出的结果就是左右中了
 func postOrder(root *Node) []int {
 	ret := []int{}
 	stack := make([]*Node, 0, 100)
@@ -35,10 +33,10 @@ func postOrder(root *Node) []int {
 	stack = append(stack, root)
 
 	for len(stack) != 0 {
-		ns := stack[len(stack) - 1]
+		ns := stack[len(stack)-1]
 		ret = append(ret, ns.Val)
-		stack = stack[:len(stack) - 1]
-		for _,n := range ns.Children{
+		stack = stack[:len(stack)-1]
+		for _, n := range ns.Children {
 			stack = append(stack, n)
 		}
 	}
@@ -47,8 +45,8 @@ func postOrder(root *Node) []int {
 	return ret
 }
 
-func reverse(s []int){
-	for i,j := 0,len(s) - 1; i < j; i,j = i+1,j-1{
-		s[i],s[j] = s[j],s[i]
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
 	}
 }
